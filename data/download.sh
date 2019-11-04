1dca2d5490355a4803a1d356f03080c99907ddd2
root <root@rsna.darragh.com>
2019-11-04 20:24:57 +0000

    Wrapping up steps

---
+++ data/download.sh
@@ -0,0 +1,4 @@
+kaggle competitions download -c rsna-intracranial-hemorrhage-detection
+unzip rsna-intracranial-hemorrhage-detection
+python rsna/eda/window_meta2csv.py
+python rsna/eda/window_1.py 
