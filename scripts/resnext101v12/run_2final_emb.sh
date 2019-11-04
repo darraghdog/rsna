9101f4067ad89de1d522f6280dd72a083315312f
darraghdog <darragh.hanley@gmail.com>
2019-11-03 20:31:26 -0600

    Resnext v12

--- scripts/resnext101v12/run_2final_emb.sh
+++ scripts/resnext101v12/run_2final_emb.sh
@@ -1,4 +1,4 @@
-N_GPU=3
+N_GPU=4
 WDIR='resnext101v12'
 FOLD=0
 SIZE='480'
@@ -7,14 +7,14 @@ for FOLD in  0 1 2
 do  
     for HFLIP in F T
     do
-        bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
-            -m dbslp1897 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainorig.py  \
-            --logmsg Rsna-lb-$SIZE-fp16 --start 0 --epochs 6 --fold $FOLD  --lr 0.00002 --batchsize 64  --workpath scripts/$WDIR  \
+        bsub  -q priority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
+            -m dbslp1827 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainorig.py  \
+            --logmsg Rsna-lb-$SIZE-fp16 --start 0 --epochs 2 --fold $FOLD  --lr 0.00002 --batchsize 64  --workpath scripts/$WDIR  \
             --hflip $HFLIP --transpose F --infer EMB --imgpath data/mount/512X512X6/ --size $SIZE --weightsname weights/model_512_resnext101$FOLD.bin"
     done
 
-    bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
-            -m dbslp1897 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainorig.py  \
-            --logmsg Rsna-lb-$SIZE-fp16 --start 0 --epochs 6 --fold $FOLD  --lr 0.00002 --batchsize 64  --workpath scripts/$WDIR  \
+    bsub  -q normal -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
+            -m dbslp1828 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainorig.py  \
+            --logmsg Rsna-lb-$SIZE-fp16 --start 0 --epochs 4 --fold $FOLD  --lr 0.00002 --batchsize 32  --workpath scripts/$WDIR  \
             --hflip F --transpose T --infer EMB --imgpath data/mount/512X512X6/ --size $SIZE --weightsname weights/model_512_resnext101$FOLD.bin"
 done
--- scripts/resnext101v12/run_train1024lstmdeltaTsum.sh
+++ scripts/resnext101v12/run_train1024lstmdeltaTsum.sh
@@ -4,9 +4,9 @@ FOLD=0
 SIZE='480'
 GEPOCH=1
 
-for GEPOCH in   0 1 2 3 4 5 #  6 7 8
+for GEPOCH in   0 1 2 3 4  # 5 6 7 8
 do
-    for FOLD in 0 # 1 2
+    for FOLD in  1 2 # 0 
     do
         bsub  -q normal  -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
             -m dbslp1828  -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainlstmdeltasum.py  \
