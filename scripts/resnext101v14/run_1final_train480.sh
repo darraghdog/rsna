1d27f95eb5c12a04e2235f17b4798a430c7a3da5
darraghdog <darragh.hanley@gmail.com>
2019-11-16 10:11:59 -0600

    Singl model training

--- scripts/resnext101v14/run_1final_train480.sh
+++ scripts/resnext101v14/run_1final_train480.sh
@@ -3,7 +3,7 @@ WDIR='resnext101v14'
 FOLD=0
 SIZE='480'
 
-for FOLD in 2 # 0 1 2 
+for FOLD in 0 # 0 1 2 
 do
     bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:apex_build \
             -m dbslp1829 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/rsna/scripts/$WDIR  && python3 trainorig.py  \
